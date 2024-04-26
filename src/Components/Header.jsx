a7d2899d195810cb6721b412275bde132d19c860
MungaiKeren <wambukeren@gmail.com>
2024-04-26 15:11:52 +0300

    Update dependencies and import statements

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,9 +1,35 @@
 import React from "react";
+import logo from "../logo.jpg";
+import { useTranslation } from "react-i18next";
 
 export default function Header() {
+  const { t, i18n } = useTranslation();
+
+  const changeLanguage = (language) => {
+    i18n.changeLanguage(language);
+  };
+
+
+
   return (
-    <div>
-      <h1>Header</h1>
-    </div>
+    <>
+      <nav className="navbar navbar-light bg-light">
+        <div className="container">
+          <a href="#" className="navbar-brand">
+            <img src={logo} alt="logo" width="50" height="50" />
+            Weather App
+          </a>
+          <div className="d-flex">
+            <select
+              className="form-select"
+              onChange={(e) => changeLanguage(e.target.value)}
+            >
+              <option value="en">English</option>
+              <option value="sw">Kiswahili</option>
+            </select>
+          </div>
+        </div>
+      </nav>
+    </>
   );
 }
--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,12 +1,20 @@
 import React from "react";
 import Header from "../Components/Header";
+import { useTranslation } from "react-i18next";
 
 
 export default function HomePage() {
+  const { t } = useTranslation();
+  const lng = navigator.language;
+
   return (
-    <div>
+    <>
       <Header />
-      <h1>Home Page</h1>
-    </div>
+      <h1>{t("hello")}</h1>
+      <p>{t("cancel")}</p>
+      <p>{t("continue")}</p>
+
+      <p>Browser Language: {lng}</p>
+    </>
   );
 };
