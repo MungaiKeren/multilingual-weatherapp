96091fa2dff202048858ed9594e35b4a377b90f3
MungaiKeren <wambukeren@gmail.com>
2024-04-26 12:17:12 +0300

    setup

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import Header from "../Components/Header";
 
 
 export default function HomePage() {
   return (
     <div>
+      <Header />
       <h1>Home Page</h1>
     </div>
   );
