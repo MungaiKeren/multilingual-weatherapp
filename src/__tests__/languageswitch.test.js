36a72d430ea57ab576fad4de7a1075b77ffec0b3
MungaiKeren <wambukeren@gmail.com>
2024-04-29 16:54:54 +0300

    remove comments

--- src/__tests__/languageswitch.test.js
+++ src/__tests__/languageswitch.test.js
@@ -2,16 +2,14 @@ import React from 'react';
 import { render, fireEvent, screen, waitFor } from '@testing-library/react';
 import Header from '../Components/Header'; 
 import { I18nextProvider } from 'react-i18next';
-import i18n from '../i18n'; // Corrected path to i18n instance
+import i18n from '../i18n';
 
-// Mock the entire i18next module
 jest.mock('i18next', () => ({
   use: jest.fn(),
   init: jest.fn(),
   t: jest.fn(),
 }));
 
-// Define mock translations
 const translations = {
   en: {
     Title: 'Weather App',
@@ -31,13 +29,10 @@ render(
     </I18nextProvider>
 );
 
-// Initial text before language change
 expect(screen.getByText('Weather App')).toBeInTheDocument();
 
-// Change language to Kiswahili
 fireEvent.change(screen.getByLabelText('Change Language'), { target: { value: 'sw' } });
 
-// Wait for translation to be loaded
 const translatedText = await screen.findByText('Programu ya hali ya hewa');
 expect(translatedText).toBeInTheDocument();
 });
